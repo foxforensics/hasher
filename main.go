053a9960bdaa66e081f5a299bf7e1c916a5a686a
Christian Uhsat <christian@uhsat.de>
2026-05-31 23:03:49 +0200

    fix: usage and deps

--- main.go
+++ main.go
@@ -2,11 +2,11 @@
 //
 // Usage:
 //
-//	hasher algo path
+//	hasher algorithm path
 //
 // The arguments are:
 //
-//	algo
+//	algorithm
 //		    Hash algorithm to used (required).
 //	path
 //		    File or folder to hash (required).
@@ -17,16 +17,41 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"go.foxforensics.dev/go-mmap"
 	"go.foxforensics.dev/hasher/hash"
 )
 
+var Usage = `© 2026 Fox Forensics. Licensed under MIT License.
+Usage: hasher ALGORITHM PATH
+
+Cryptographic hashes:
+  BLAKE2S-256, BLAKE2B-256, BLAKE2B-384, BLAKE2B-512, BLAKE3-256, BLAKE3-512
+  GOST2012-256, GOST2012-512, HAS-160, LSH-256, LSH-512, MD2, MD4, MD5, MD6
+  RIPEMD-160, SHAKE128, SHAKE256, SHA1, SHA224, SHA256, SHA512, SHA3, SHA3-224
+  SHA3-256, SHA3-384, SHA3-512, Skein-224, Skein-256, Skein-384, Skein-512, SM3
+  Whirlpool
+
+Performance hashes:
+  DJB2, FNV-1, FNV-1a, Murmur3, RapidHash, SipHash, XXH32, XXH64, XXH3
+
+Perceptual hashes:
+  Average, Difference, Median, PHash, WHash, MarrHildreth, BlockMean, PDQ, RASH
+
+Similarity hashes:
+  ImpFuzzy, ImpHashO, ImpHashS, SSDeep, TLSH
+
+Windows hashes:
+  LM, NT, PE
+
+Checksums:
+  Adler32, Fletcher4, CRC16-CCITT, CRC32-C, CRC32-IEEE, CRC64-ECMA, CRC64-ISO
+
+Report bugs at: foxforensics.dev/issues`
+
 func main() {
 	if len(os.Args) < 3 || os.Args[1] == "--help" {
-		_, _ = fmt.Fprintln(os.Stderr, "usage: hasher algo path\n--")
-		_, _ = fmt.Fprintln(os.Stderr, strings.Join(hash.Algorithms, ", "))
+		_, _ = fmt.Fprintln(os.Stderr, Usage)
 		os.Exit(2)
 	}
 
